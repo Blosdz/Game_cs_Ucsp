6649cc890916a664e21e1b206f4199b977cedaa0
[johan jimenez] <[johan_sokar@hotmail.com]>
2020-11-13 23:23:43 -0500

    entendiendo_sfml

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,13 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1200, 600), "The zelda",sf::Style::Close|sf::Style::Titlebar);
     /*sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-*/
+*/  
+    window.setFramerateLimit(60);
+    sf::RectangleShape rectangle(sf::Vector2f(100.0f,100.0f));
+    rectangle.setFillColor(sf::Color::Blue);
+
+    rectangle.setPosition(440,300);
+
     sf::VideoMode::getDesktopMode();
     while (window.isOpen())
     {
@@ -14,10 +20,16 @@ int main()
         {
             if (evnt.type == sf::Event::Closed)
                 window.close();
+
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+
+                rectangle.move(0,5);
+            }
         }
 
         window.clear();
         /*window.draw(shape);*/
+        window.draw(rectangle);
         window.display();
     }
 
